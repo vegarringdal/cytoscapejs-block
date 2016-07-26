be8981d8b523bb730381c179f5bab9e0f9d95098
vegarringdal <vegar.ringdal@gmail.com>
2016-07-26 16:54:55 +0200

    chore(demo)

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import 'bootstrap';
 export function configure(aurelia) {
   aurelia.use
     .standardConfiguration()
-    .developmentLogging()
+    //.developmentLogging()
     .plugin('aurelia-v-grid')
 
   //Uncomment the line below to enable animation.
