317edcdd2c6e514cb207f12e6744d4499d7aee14
vegarringdal <vegar.ringdal@gmail.com>
2016-07-26 16:38:36 +0200

    chore(demo)

---
+++ jspm_packages/github/aurelia-ui-toolkits/aurelia-v-grid@0.2.0.js
@@ -0,0 +1,3 @@
+define(["github:aurelia-ui-toolkits/aurelia-v-grid@0.2.0/index"], function(main) {
+  return main;
+});
---
+++ jspm_packages/npm/aurelia-framework@1.0.0-rc.1.0.11.js
@@ -0,0 +1,3 @@
+define(["npm:aurelia-framework@1.0.0-rc.1.0.11/aurelia-framework"], function(main) {
+  return main;
+});
